63faa995402136b0b6b3a0859cc18ee068e2d044
Zayd123mouses <90765041+Zayd123mouses@users.noreply.github.com>
2022-09-24 23:23:54 +0200

    add, delete and update books categories for users

--- frontend/books/src/services/apiList.js
+++ frontend/books/src/services/apiList.js
@@ -18,6 +18,26 @@
 // API to create new users
 "http://127.0.0.1:8000/users/register/"
 //  must provide username , password, and confirmation password, firstName, lastName
-// still need to be tested
+
+
+// API to get list of books of particular user
+"http://127.0.0.1:8000/books-api/books/"
+// { book_title, book_image, book_state, book_id, user_id}  this what will come back
+// this endpoint for authiticated users only 
+// we will sort each book in it is categorie based on the state that cameback
+
+// to add new book , send post request with this data  {book_title, book_id, book_image, book_state, user_id}
+
+// username = testuser , password = helloworld999 , you can use this user to test some functionality or create user yourself
+
+
+// API to update the book state 
+// "http://127.0.0.1:8000/books-api/books/${book_id}" , book_id is the id we get from google api search
+
+// book_state must be one of three values : toRead, reading, read
+// for now we must send the data as follows
+// {book_title, book_id, book_image, book_state, user_id}
+// maybe we will change afterwords so we no longer have to send the user_id as will , but let's work with this for now
+// this endpoint support delete as well
 
 
