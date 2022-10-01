16c4e02596c0a5d7df3afcc8bde641f976092547
pablo-maff <pablo.maff.076@gmail.com>
2022-10-01 17:24:18 +0200

    Homepage Layout

--- frontend/books/src/componets/HomePage.js
+++ frontend/books/src/componets/HomePage.js
@@ -1,22 +1,209 @@
-import reading_pic from '../images/reading_books.png'
-import {Link} from "react-router-dom"
-import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Box, Button, Grid, Typography } from '@mui/material'
 // basic homepage that still need styling.
-const HomePage = ()=>{
-    const user = useSelector(state=>state.user)
-
-    
-    return(
-        <div className="improtant">
-            {user ? <h1>Hello ---{user.username}</h1> : ''}
-
-            <h2 >The importance of reading books</h2>
-            <Link to='/search'>Search here</Link>
-            <section>
-                <img src={reading_pic} width='30%'/>
-            </section>
-        </div>
-    )
+const HomePage = () => {
+  //TODO: Improve typography
+  //TODO: Use theme in index.js to generalize some customized components
+  //TODO: Create footer
+  //TODO: Change grid containers to work with flexbox instead of grid values
+  return (
+    <>
+      <Box sx={{ mt: '80px' }}>
+        <Grid container spacing={22}>
+          {/* <Box sx={{ display: 'flex', mt: '100px' }}> */}
+          <Grid item xs={6} sx={{ mt: 6 }}>
+            <Typography variant='myVariant'>
+              Find the books you are looking for anywhere
+            </Typography>
+            <Typography variant='body1' sx={{ mt: 6, fontSize: 18 }}>
+              Search for books anywhere, you don't need to know the name of the
+              book, you can search by author or publisher, just type and search.
+            </Typography>
+            {/* sx: p:2 = padding = 2 * default theme spacing(8px) = 16 px. To scale up or down you can change default theme spacing */}
+            <Button
+              component={Link}
+              to='/search'
+              sx={{ mt: 6, py: 2, px: 12 }}
+              variant='contained'
+            >
+              Search now
+            </Button>
+          </Grid>
+          <Grid item xs={4}>
+            <Box
+              component='img'
+              alt='People searching books on laptops'
+              src='https://raw.githubusercontent.com/LuisFrag/booksure/c60a884400543991c0b62ae42af2afbb00203d98/src/assets/svg/main-apresentation.svg'
+            />
+          </Grid>
+        </Grid>
+      </Box>
+      <Box>
+        <Box
+          sx={{
+            height: '32rem',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography variant='h4' sx={{ width: 600, textAlign: 'center' }}>
+            THE IMPORTANCE OF READING BOOKS
+          </Typography>
+        </Box>
+      </Box>
+      <Box sx={{ mb: 16 }}>
+        <Grid container spacing={22}>
+          <Grid item xs={8} sx={{ mt: 8 }}>
+            <Typography variant='h5'>
+              READING PRACTICE IMPROVES VOCABULARY
+            </Typography>
+            <Typography
+              variant='body1'
+              sx={{ mt: 6, fontSize: 18, marginRight: 45 }}
+            >
+              Whether for pleasure, study or information, reading practice
+              improves vocabulary and streamlines reasoning and interpretation.
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Box
+              component='img'
+              alt='Person reading a book'
+              src='https://raw.githubusercontent.com/LuisFrag/booksure/c60a884400543991c0b62ae42af2afbb00203d98/src/assets/svg/home-1.svg'
+            />
+          </Grid>
+        </Grid>
+      </Box>
+      <Box sx={{ mb: 16 }}>
+        <Grid container spacing={22}>
+          <Grid item xs={4}>
+            <Box
+              component='img'
+              alt='Person reading a book'
+              src='https://raw.githubusercontent.com/LuisFrag/booksure/c60a884400543991c0b62ae42af2afbb00203d98/src/assets/svg/home-2.svg'
+            />
+          </Grid>
+          <Grid item xs={8} sx={{ mt: 8 }}>
+            <Typography variant='h5' align='right'>
+              ADVANCING TECHNOLOGY
+            </Typography>
+            <Typography
+              variant='body1'
+              align='right'
+              sx={{ mt: 6, fontSize: 18, marginLeft: 45 }}
+            >
+              With the advancement of technologies in the modern world, people
+              are less and less interested in reading.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box sx={{ mb: 16 }}>
+        <Grid container spacing={22}>
+          <Grid item xs={8} sx={{ mt: 8 }}>
+            <Typography variant='h5'>DYNAMIC READING</Typography>
+            <Typography
+              variant='body1'
+              sx={{ mt: 6, fontSize: 18, marginRight: 45 }}
+            >
+              Dynamic and relaxed reading is one of the best ways to acquire
+              information. The ideal is to learn to read informative texts,
+              scientific articles, textbooks, educational books, etc.
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Box
+              component='img'
+              alt='Person sitting on a couch while using a laptop'
+              src='https://raw.githubusercontent.com/LuisFrag/booksure/c60a884400543991c0b62ae42af2afbb00203d98/src/assets/svg/home-3.svg'
+            />
+          </Grid>
+        </Grid>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexWrap: 'wrap',
+          bgcolor: '#a2d2ff',
+          height: 500,
+        }}
+      >
+        <Typography variant='h3' sx={{ mx: 25, textAlign: 'center' }}>
+          ALL THE BOOKS YOU LOOK FOR IN ONE PLACE
+        </Typography>
+        <Box
+          component='img'
+          alt='Person sitting on a pile of books while reading on a laptop'
+          src='https://raw.githubusercontent.com/LuisFrag/booksure/c60a884400543991c0b62ae42af2afbb00203d98/src/assets/svg/home-4.svg'
+        />
+      </Box>
+      <Box sx={{ mb: 20 }}>
+        <Grid container direction='row' justifyContent='space-around'>
+          <Grid item>
+            <Box
+              component='img'
+              alt='Person sitting on a pile of books while reading on a laptop'
+              src='https://raw.githubusercontent.com/LuisFrag/booksure/c60a884400543991c0b62ae42af2afbb00203d98/src/assets/svg/home-5.svg'
+            />
+            <Typography
+              variant='body1'
+              sx={{ fontSize: 18, maxWidth: '18rem', textAlign: 'center' }}
+            >
+              Download samples in PDF or EPUB of your favorite books
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Box
+              component='img'
+              alt='Person sitting on a pile of books while reading on a laptop'
+              src='https://raw.githubusercontent.com/LuisFrag/booksure/c60a884400543991c0b62ae42af2afbb00203d98/src/assets/svg/home-6.svg'
+            />
+            <Typography
+              variant='body1'
+              sx={{ fontSize: 18, maxWidth: '18rem', textAlign: 'center' }}
+            >
+              Buy the books you're looking for from your phone or PC
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box>
+        <Typography variant='h5' textAlign='center'>
+          THE TECHNOLOGIES USED
+        </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignContent: 'center',
+            mb: 10,
+          }}
+        >
+          <Box
+            component='img'
+            alt='React logo'
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png'
+            sx={{ maxWidth: '65px', maxHeight: '65px', m: 2 }}
+          />
+          <Box
+            component='img'
+            alt='Django logo'
+            src='https://cdn.iconscout.com/icon/free/png-256/django-13-1175187.png'
+            sx={{ maxWidth: '65px', maxHeight: '65px', m: 2 }}
+          />
+          <Box
+            component='img'
+            alt='Google logo'
+            src='https://raw.githubusercontent.com/LuisFrag/booksure/c60a884400543991c0b62ae42af2afbb00203d98/src/assets/svg/google.svg'
+            sx={{ maxWidth: '65px', maxHeight: '65px', m: 2 }}
+          />
+        </Box>
+      </Box>
+    </>
+  )
 }
 
 export default HomePage
--- frontend/books/src/index.js
+++ frontend/books/src/index.js
@@ -1,22 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
-import {BrowserRouter as Router} from "react-router-dom"
-import { store } from './store';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { store } from './store'
+import { createTheme, ThemeProvider } from '@mui/material'
+import { red } from '@mui/material/colors'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: '#CCC',
+    },
+    myCustomColor: {
+      main: red[400],
+      superDark: red[800],
+      superLight: red[100],
+    },
+  },
+  typography: {
+    myVariant: {
+      // I can pass myVariant as variant instead of h1,h2, etc.
+      fontSize: '3rem',
+    },
+  },
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-  <Router>
-    <Provider store={store}> <App /> </Provider>
-  </Router>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ThemeProvider>
+    </Router>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
