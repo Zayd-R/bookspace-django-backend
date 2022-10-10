336e37730f5cddd27fd8f165261288bb5e3ec18f
Zayd123mouses <theking.zaidyou@gmail.com>
2022-10-10 15:40:59 +0200

    kk

--- src/services/register.js
+++ src/services/register.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = '/users/register/'
+const baseUrl = 'http://127.0.0.1:8000/users/register/'
 
 const register = async (newUser) => {
   const results = await axios.post(baseUrl, newUser)
